53789982a12e95ef1d74fec7458c11e4bab7dd0b
Lucas Araujo <lucasdearaujor@gmail.com>
2025-04-14 16:12:56 -0300

    first commit

---
+++ app/src/main/java/br/com/lucasaraujo/trabalhopratico03crudusuario/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.lucasaraujo.trabalhopratico03crudusuario
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.lucasaraujo.trabalhopratico03crudusuario.ui.theme.TrabalhoPratico03CrudUsuarioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TrabalhoPratico03CrudUsuarioTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TrabalhoPratico03CrudUsuarioTheme {
+        Greeting("Android")
+    }
+}
